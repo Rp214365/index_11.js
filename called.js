52cf1f1c367e57e58e816af19bee44d27efd72f9
Rp214365 <patelricky7801@gmail.com>
2024-07-13 20:33:52 +0530

    Add files via upload

---
+++ called.js
@@ -0,0 +1,40 @@
+// let val1 = 10;
+// let val2 = 5;
+
+// function addnum(num1,num2){
+//     let total = num1+num2
+
+//     return total 
+// }
+
+// let resunt1 = addnum( val1,val2);
+// let resunt2 = addnum(10,2);
+
+// console.log(resunt1);
+// console.log(resunt2);
+
+function one() {
+    console.log("one");
+    there()
+}
+
+function two() {
+    console.log("two");
+    one()
+}
+
+function there() {
+    console.log("there")
+}
+
+one()
+two()
+there()
+
+// Can you Explain this code in Gujarati ?
+// one
+// there
+// two
+// one
+// there
+// there
